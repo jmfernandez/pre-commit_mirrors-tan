c4ceb2f07e077e477c0144f86f9c31b5455c8a68
José María Fernández <jose.m.fernandez@bsc.es>
2022-10-06 01:24:57 +0200

    Mirror: 20.8.2

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ from setuptools import setup
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['tan==20.8.1'],
+    install_requires=['tan==20.8.2'],
 )
