26cc00b64c79b4f013990f63dd1e44f4c5cd56db
José Mª Fernández <jose.m.fernandez@bsc.es>
2023-06-20 16:36:18 +0200

    Now, really, version bump to 23.3.0, synchronized with the contents of `.version` file

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup
 
-
+import os.path
+tan_version = "0.0.0"
+with open(os.path.join(os.path.dirname(__file__), ".version")) as vH:
+    for line in vH:
+        tan_version = line.strip()
+        break
 setup(
     name='pre_commit_placeholder_package',
     version='0.0.0',
-    install_requires=['tan==22.8.0'],
+    install_requires=[f'tan=={tan_version}'],
 )
